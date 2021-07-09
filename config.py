81757a66838ac67639f620cbac7db525ecdac510
chentong-dev <chen.yinren@163.com>
2021-07-09 09:24:43 +0800

    修改: 数据库 配置项从环境变量读取

--- config.py
+++ config.py
@@ -5,6 +5,7 @@
 
 
 import logging
+import os
 
 
 # 基本配置信息
@@ -12,10 +13,10 @@ class Config(object):
     DEBUG = True
 
     # 数据库配置
-    MYSQL_HOST = "localhost"
-    MYSQL_PORT = "3306"
-    MYSQL_USER = "root"
-    MYSQL_PASSWORD = "123456"
+    MYSQL_HOST = os.environ.get('FLASK_MYSQL_HOST') or "localhost"
+    MYSQL_PORT = os.environ.get('FLASK_MYSQL_PORT') or "3306"
+    MYSQL_USER = os.environ.get('FLASK_MYSQL_USER') or "root"
+    MYSQL_PASSWORD = os.environ.get('FLASK_MYSQL_PASSWORD') or "123456"
     DATABASE = "test"
     SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{DATABASE}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -23,9 +24,9 @@ class Config(object):
 
     # flask缓存配置
     CACHE_TYPE = "RedisCache"
-    CACHE_REDIS_HOST = "127.0.0.1"
-    CACHE_REDIS_PORT = 6379
-    CACHE_REDIS_PASSWORD = "123456"
+    CACHE_REDIS_HOST = os.environ.get('FLASK_REDIS_HOST') or "127.0.0.1"
+    CACHE_REDIS_PORT = os.environ.get('FLASK_REDIS_PORT') or 6379
+    CACHE_REDIS_PASSWORD = os.environ.get('FLASK_REDIS_PASSWORD') or "123456"
     CACHE_REDIS_DB = 0
 
     # 默认日志等级
